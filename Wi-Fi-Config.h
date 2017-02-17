5276d7c0b293e93dfe778314f530f22916b3a1b5
John M. Wargo <john@johnwargo.com>
2017-02-17 11:31:24 -0500

    updated Fri 02/17/2017 11:31:24.73

--- Wi-Fi-Config.h
+++ Wi-Fi-Config.h
@@ -1,5 +1,5 @@
 // Populate the defines below with the settings for your Wi-Fi network
 //  Network SSID (name)
-#define wifi_ssid "Wargo"
+#define wifi_ssid ""
 // SSID password (use for WPA, or use as key for WEP)
-#define wifi_pass "nothing is ever easy"
+#define wifi_pass ""
